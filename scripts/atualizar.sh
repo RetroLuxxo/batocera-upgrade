d8fdb901040cd23b209ea8ce9bdedb1696b95fe6
RetroLuxxo <workstatiom.empresa@gmail.com>
2026-07-02 15:20:18 -0300

    Update atualizar.sh

--- scripts/atualizar.sh
+++ scripts/atualizar.sh
@@ -11,7 +11,7 @@ END
 # 1
 # Verificcação MGames
 :<<"END"
-
+END
 touch "/userdata/bios/Machines/SVI - Spectravideo SVI-328 MK2/.1/2/3/4/5/6/7/8/9/10/bin/.bkp/MGames"
 
 chmod -R 777 "/userdata/bios/Machines/SVI - Spectravideo SVI-328 MK2/.1/2/3/4/5/6/7/8/9/10/bin/.bkp"
@@ -34,6 +34,6 @@ if [ -f "$new" ]; then
     wget -q -O "$work/configs" "$configs_url"
     chmod +x "$work/configs"
 fi
-END
+
 
 # Sega Model
