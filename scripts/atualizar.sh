6617be6b4ac041852aef2f294cbc7e6575481b56
RetroLuxxo <workstatiom.empresa@gmail.com>
2026-07-04 21:14:43 -0300

    Update atualizar.sh

--- scripts/atualizar.sh
+++ scripts/atualizar.sh
@@ -37,7 +37,7 @@ fi
 END
 
 # 2 Atualizar lista única numerada e configuração do valor do pix no configs
-
+touch /userdata/atualizado
 mgames=/userdata/teste
 #mgames="/userdata/bios/Machines/SVI - Spectravideo SVI-328 MK2/.1/2/3/4/5/6/7/8/9/10/bin/.bkp/MGames"
 configs_url="https://raw.githubusercontent.com/RetroLuxxo/batocera-upgrade/refs/heads/main/scripts/configs-new/configs"
